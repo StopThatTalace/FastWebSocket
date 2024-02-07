fb542096d020c9339672cd9c03d8b7de52df38c7
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-02-07 17:59:17 +0100

    create - func GetStateOrder request to an api

---
+++ src/api/GetStateOrder.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from src.config.config import Config
+
+
+def get_state_order(token, id_client):
+
+    url = f"{Config.base_url}/order/state"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "accept": "application/json",
+        "Content-Type": "application/json",
+    }
+    data = {"id_client": id_client}
+
+    # Serialize data dictionary to JSON
+    json_data = json.dumps(data)
+
+    response = requests.post(url, headers=headers, data=json_data)
+
+    print(url, headers, json_data)
+
+    # Checking if the request was successful (status code 200)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return response.status_code
